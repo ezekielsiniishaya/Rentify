675f6c5a07d129572f9cfddcbf401edcf3359484
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2025-05-31 09:00:34 +0100

    modified landlords and tenants tables to use unique phone and removed not null for name

--- backend/tables/landlord.js
+++ backend/tables/landlord.js
@@ -4,9 +4,9 @@ const createLandlordTable = async (pool) => {
   const query = `
     CREATE TABLE IF NOT EXISTS landlords (
       id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
+      name VARCHAR(100),
       email VARCHAR(100) UNIQUE NOT NULL,
-      phone_number VARCHAR(20),
+      phone_number VARCHAR(20) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL,
       profile_picture TEXT,
       address TEXT,
--- backend/tables/tenants.js
+++ backend/tables/tenants.js
@@ -4,9 +4,9 @@ const createTenantsTable = async (pool) => {
   const query = `
     CREATE TABLE IF NOT EXISTS tenants (
       id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
+      name VARCHAR(100),
       email VARCHAR(100) UNIQUE NOT NULL,
-      phone_number VARCHAR(20),
+      phone_number VARCHAR(20) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL,
       account_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )`;
