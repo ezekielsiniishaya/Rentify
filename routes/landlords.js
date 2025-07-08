743aa3f2c1230a9bb7d0e6cbcf918afcf77a5f86
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2025-07-08 09:40:03 +0100

    updated email verification

--- routes/landlords.js
+++ routes/landlords.js
@@ -139,12 +139,12 @@ router.get("/verify-email", async (req, res) => {
 
   if (updateError) {
     return res.redirect(
-      "https://rentifyapp.netlify.app/login.html?message=Verification%20failed"
+      "https://rentifyapp.netlify.app/pages/login.html?message=Verification%20failed"
     );
   }
 
   return res.redirect(
-    "https://rentifyapp.netlify.app/login.html?message=Email%20successfully%20verified"
+    "https://rentifyapp.netlify.app/pages/login.html?message=Email%20successfully%20verified"
   );
 });
 
