a0834a388dab924e67c694f68f6b0c900b3a2f49
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2025-07-08 10:19:16 +0100

    updated email verification

--- routes/landlords.js
+++ routes/landlords.js
@@ -144,11 +144,11 @@ router.get("/verify-email", async (req, res) => {
     );
   }
 
-  res.redirect(
-    `https://rentify-frontend-production.up.railway.app/pages/login.html?message=${encodeURIComponent(
-      "Email successfully verified"
-    )}`
-  );
+  res.status(201).json({
+    redirect: `https://rentify-frontend-production.up.railway.app/login.html?message=${encodeURIComponent(
+      "Please verify your email before logging in"
+    )}`,
+  });
 });
 
 // POST /api/landlords/login
